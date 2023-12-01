7e6d3b5eb27298390edff2d5b42d096b96afe637
Aidan Shilling <aidanshilling91@gmail.com>
2023-12-01 17:19:48 -0600

    Added input.txt and file reading

--- day-1/trebuchet-rs/src/main.rs
+++ day-1/trebuchet-rs/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_name = "input.txt";
+    let contents = fs::read_to_string(file_name)
+        .expect("Should have been able to read file");
+
+    println!("{contents}");
 }
