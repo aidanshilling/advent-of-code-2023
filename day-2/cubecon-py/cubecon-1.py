3c44d52bc4868e5ff8af15da7df84f83e499f53e
Aidan Shilling <aidanshilling@disroot.org>
2024-09-25 13:56:07 -0500

    Add solution in python for day2 p1

--- day-2/cubecon-py/cubecon-1.py
+++ day-2/cubecon-py/cubecon-1.py
@@ -1,11 +1,9 @@
-def parse_round(round):
-
+def check_round_validity(round):
     key = {
         'red': 12,
         'green': 13,
         'blue': 14
     }
-
     rolls = round.split(',')
     for roll in rolls:
         count, color = roll.lstrip().strip().split(' ')
@@ -14,19 +12,59 @@ def parse_round(round):
     return True
 
 
+def check_game_validity(rounds):
+    for round in rounds.split(';'):
+        if not check_round_validity(round):
+            return False
+    return True
+
+
+def get_round_min(round):
+    rolls = round.split(',')
+    min = {
+        'red': 0,
+        'green': 0,
+        'blue': 0
+    }
+    for roll in rolls:
+        count, color = roll.lstrip().strip().split(' ')
+        min[color] += int(count)
+    return min
+
+
+def get_game_min(rounds):
+    min = {
+        'red': 0,
+        'green': 0,
+        'blue': 0
+    }
+    for round in rounds.split(';'):
+        round_min = get_round_min(round)
+        if min['red'] < round_min['red']:
+            min['red'] = round_min['red']
+        if min['green'] < round_min['green']:
+            min['green'] = round_min['green']
+        if min['blue'] < round_min['blue']:
+            min['blue'] = round_min['blue']
+    return min
+
+
+def get_power_set(d):
+    return d['red'] * d['green'] * d['blue']
+
+
 if __name__ == "__main__":
     with open('./input', 'r') as f:
         good_games = []
+        power_sets = []
         for line in f:
-            bad_game = False
             game = line.strip()
             id, rounds = game.split(':')
             id = id.split(' ')[1]
-            for round in rounds.split(';'):
-                if not parse_round(round):
-                    bad_game = True
-                    break
-            if not bad_game:
+            if check_game_validity(rounds):
                 good_games.append(int(id))
-
+            game_min = get_game_min(rounds)
+            power_sets.append(get_power_set(game_min))
         print("Part 1 answer: {}".format(sum(good_games)))
+        print("Part 2 answer: {}".format(sum(power_sets)))
+
