666fcdd2bdd5b9afcbfe5fefcf109316eda1d970
Aidan Shilling <aidanshilling@disroot.org>
2024-09-26 18:05:39 -0500

    Add day3 p1 solution

--- day-3/gear-ratios-py/gear.py
+++ day-3/gear-ratios-py/gear.py
@@ -11,9 +11,8 @@ def read_schematic_into_matrix(schematic):
         '8',
         '9',
     )
-
     matrix = []
-    symbols = []
+    symbols = {}
     numbers = []
     with open(schematic, 'r') as f:
         i = 0
@@ -22,13 +21,19 @@ def read_schematic_into_matrix(schematic):
             j = 0
             for char in line.strip():
                 if char in valid_digits:
-                    if len(numbers) > 0 and numbers[-1][1][1] == j-1:
-                        numbers[-1][1] = (i, j)
-                        numbers[-1][2] = numbers[-1][2] + char
+                    if len(numbers) > 0 and numbers[-1]['end']['j'] == j-1:
+                        numbers[-1]['end'] = {'i': i, 'j': j}
+                        numbers[-1]['value'] = numbers[-1]['value'] + char
                     else:
-                        numbers.append([(i, j), (i, j), char])
+                        numbers.append({
+                            'start': {'i': i, 'j': j},
+                            'end': {'i': i, 'j': j},
+                            'value': char
+                        })
                 elif char != '.':
-                    symbols.append((i, j))
+                    symbols["({}, {})".format(i, j)] = True
+                else:
+                    symbols["({}, {})".format(i, j)] = False
                 row.append(char)
                 j += 1
             matrix.append(row)
@@ -36,23 +41,56 @@ def read_schematic_into_matrix(schematic):
     return matrix, numbers, symbols
 
 
-def find_valid_part_numbers(matrix, symbols):
+def get_surrounding_coords(num, w, h):
+    i = num['start']['i']
+    j1 = num['start']['j']
+    j2 = num['end']['j']
+
+    coords = []
+    if j1-1 >= 0:
+        start = j1-1
+        coords.append((i, j1-1))
+    else:
+        start = 0
+    if j2+1 < w-1:
+        stop = j2+2
+        coords.append((i, j2+1))
+    else:
+        stop = w-1
+
+    for x in range(start, stop):
+        if i != 0:
+            coords.append((i-1, x))
+        if i < h-1:
+            coords.append((i+1, x))
+
+    return coords
+
+
+def find_valid_part_numbers(matrix, numbers, symbols):
 
     # [i-1,j], [i+1,j], [i, j-1], [i, j+1],
     # [i-1, j-1], [i-1, j+1], [i+1, j-1], [i+1, j+1]
+    good_numbers = []
+    h = len(matrix)
+    w = len(matrix[0])
 
-    for (i, j) in symbols:
+    for num in numbers:
         # will need to account for being on the edges of the matrix
         # and when two numbers are next to eachother in the same row
         # (they would be part of the same number)
-        print(matrix[i][j])
+        coords = get_surrounding_coords(num, w, h)
+        for coord in coords:
+            if symbols["({}, {})".format(coord[0], coord[1])]:
+                good_numbers.append(int(num['value']))
+
+    return good_numbers
 
 
 if __name__ == "__main__":
 
     matrix, numbers, symbols = read_schematic_into_matrix('input')
-    print(symbols)
-    # find_valid_part_numbers(matrix, symbols)
+    good_numbers = find_valid_part_numbers(matrix, numbers, symbols)
 
-    answer = 0
-    print("P1 Answer: {}".format(0))
+    answer = sum(good_numbers)
+    print("P1 Answer: {}".format(answer))
