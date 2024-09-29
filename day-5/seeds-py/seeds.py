e921d75d27318abc42e484d122ee7e5ec32aa093
Aidan Shilling <aidanshilling@disroot.org>
2024-09-28 21:08:45 -0500

    Add map resolve and start of input parse

--- day-5/seeds-py/seeds.py
+++ day-5/seeds-py/seeds.py
@@ -5,11 +5,35 @@
 #       {source, dest, length}
 #   ]
 # }
+def parse_input(input):
+    result = {}
+    with open(input, 'r') as f:
+        contents = f.read()
+        sections = contents.split("\n\n")
+        for section in sections:
+            name, data = section.split(':')
+            map = create_map(data)
+            result[name] = map
 
-def map_resolver(map):
+
+def create_map(data):
     None
 
 
+def map_resolver(map):
+    result = {}
+    for line in map['lines']:
+
+        source = line['source']
+        dest = line['dest']
+        length = line['length']
+
+        for i in range(0, length):
+            result[source + i] = dest + i
+    return result
+
+
 if __name__ == "__main__":
+    parse_input('input-test')
     answer1 = 0
     print("P1 answer: {}".format(answer1))
