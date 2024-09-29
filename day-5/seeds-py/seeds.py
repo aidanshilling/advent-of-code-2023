635a1e7e24c6f09ed536d9780f9190edfc7013ab
Aidan Shilling <aidanshilling@disroot.org>
2024-09-28 20:52:23 -0500

    Add start of day5 p1 python proj

---
+++ day-5/seeds-py/seeds.py
@@ -0,0 +1,15 @@
+
+# map {
+#   name
+#   lines = [
+#       {source, dest, length}
+#   ]
+# }
+
+def map_resolver(map):
+    None
+
+
+if __name__ == "__main__":
+    answer1 = 0
+    print("P1 answer: {}".format(answer1))
